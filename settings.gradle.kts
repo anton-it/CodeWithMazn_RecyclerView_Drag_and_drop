a5915152522c77572344633fb7d0b4a14c99436d
Admin1 <a89086829981@gmail.com>
2023-11-04 13:28:21 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CodeWithMazn_RecyclerView_Drag_and_drop"
+include(":app")
+ 
